16e3dbf6c9c799bb83bee59a59119af2f6f88884
misha <milisov4@gmal.com>
2022-10-28 11:49:33 +0300

    polishing code

--- app/javascript/flashes.js
+++ app/javascript/flashes.js
@@ -1,6 +1,5 @@
 var flash_message = document.getElementById('flash-message');
 var close_button = document.getElementById('close-button');
 close_button.onclick = function() {
-  console.log('ssssss')
   flash_message.disabled = false
 };
