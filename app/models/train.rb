d61a7fc9b489da15bd1895a05ae4cd519900c830
misha <milisov4@gmal.com>
2022-11-03 14:35:36 +0200

    polishing code

--- app/models/train.rb
+++ app/models/train.rb
@@ -6,7 +6,7 @@ class Train < ApplicationRecord
   has_many :wagons
   validates_format_of :number, with: /\A[0-9a-zA-Z]{3}-{,1}[0-9a-zA-Z]{2}\z/
 
-  def count_seats(type, seat_type) 
+  def count_seats(type, seat_type)
     count = 0
     self.wagons.method(type.to_sym).call.each do |wagon|
       count += wagon.method("#{seat_type}_quantity".to_sym).call
