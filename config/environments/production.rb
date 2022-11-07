b03d159b1c9f9d5228cc342cce7b84a393b0d61d
misha <milisov4@gmal.com>
2022-11-07 15:08:46 +0200

    mistake in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = { host: 'arcane-ravine-04638.herokuapp.com/'}
-  confing.action_mailer.delivery_method = :sendgrid
+  config.action_mailer.delivery_method = :sendgrid
   config.action_mailer.sendgrid_settings = { api_token: ENV['SENDGRID_API_TOKEN'] }
 
   # Ignore bad email addresses and do not raise email delivery errors.
