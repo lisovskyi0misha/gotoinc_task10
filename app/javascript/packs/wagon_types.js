e6fd1282c3f7bc7f066371ea8c3fc57ff20da88d
misha <milisov4@gmal.com>
2022-11-08 11:20:07 +0200

    rewrote wagon_types script with jquery

--- app/javascript/packs/wagon_types.js
+++ app/javascript/packs/wagon_types.js
@@ -1,37 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
-  let coupe = document.getElementById('wagon_type_coupewagon');
-  let sleeping = document.getElementById('wagon_type_sleepingwagon');
-  let seated = document.getElementById('wagon_type_seatedwagon');
-  let economy = document.getElementById('wagon_type_economywagon');
+$(document).ready(function() {
+  let coupe = $('#wagon_type_coupewagon');
+  let sleeping = $('#wagon_type_sleepingwagon');
+  let seated = $('#wagon_type_seatedwagon');
+  let economy = $('#wagon_type_economywagon');
 
-  let coupe_seats = document.getElementById('economy-and-coupe-class-seats');
-  let economy_seats = document.getElementById('economy-class-seats')
-  let seated_seats = document.getElementById('seated-class-seats')
-  let not_sleeoing_seats = document.getElementById('not-sleeping-class-seats')
+  let coupe_seats = $('#economy-and-coupe-class-seats');
+  let economy_seats = $('#economy-class-seats')
+  let seated_seats = $('#seated-class-seats')
+  let not_sleeping_seats = $('#not-sleeping-class-seats')
 
-  coupe.onclick = function() {
-    coupe_seats.style.display = 'block'
-    not_sleeoing_seats.style.display = 'block'
-    economy_seats.style.display = 'none'
-    seated_seats.style.display = 'none'
-  };
+  coupe.click(function() {
+    coupe_seats.show();
+    not_sleeping_seats.show();
+    economy_seats.hide();
+    seated_seats.hide();
+  });
 
-  sleeping.onclick = function() {
-    coupe_seats.style.display = 'block'
-    not_sleeoing_seats.style.display = 'none'
-    economy_seats.style.display = 'none'
-    seated_seats.style.display = 'none'
-  };
+  sleeping.click(function() {
+    coupe_seats.show();
+    not_sleeping_seats.hide();
+    economy_seats.hide();
+    seated_seats.hide();
+  });
 
-  seated.onclick = function() {
-    seated_seats.style.display = 'block'
-    coupe_seats.style.display = 'none'
-  };
+  seated.click(function() {
+    seated_seats.show();
+    coupe_seats.hide();
+  });
 
-  economy.onclick = function() {
-    coupe_seats.style.display = 'block'
-    not_sleeoing_seats.style.display = 'block'
-    economy_seats.style.display = 'block'
-    seated_seats.style.display = 'none'
-  };
+  economy.click(function() {
+    coupe_seats.show();
+    not_sleeping_seats.show();
+    economy_seats.show();
+    seated_seats.hide();
+  });
 });
