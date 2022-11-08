1029094252aabd13547212f563c99c3687ee40d7
misha <milisov4@gmal.com>
2022-11-08 21:56:03 +0200

    mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'arcane-ravine-04638.herokuapp.com/' }
 
